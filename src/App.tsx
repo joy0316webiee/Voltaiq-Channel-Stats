da90f22b10fd85c8d0927e139f2d5caff057c026
joy0316webiee <joy0316webiee@gmail.com>
2023-08-02 21:42:22 -0500

    fix: absolute path config and built boilerplate of home and report pages, etc

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// pages
+import { Home, Report } from "@/pages";
+
 const App = () => {
-  return <div className="App">Channels Stats</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/report" element={<Report />} />
+
+        {/* Not Found Pages */}
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -11,10 +11,10 @@ if (process.env.NODE_ENV === "development") {
 }
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
